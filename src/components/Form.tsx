e197e20fa807ab525673fa696a486fccbc216729
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2024-08-15 21:18:17 -0600

    Añade validación en el botón de guardar

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -20,6 +20,13 @@ export default function Form() {
 		});
 	};
 
+	const isValidActivity = () => {
+		const { name, calories } = activity;
+		console.log(name.trim() !== "");
+
+		return name.trim() !== "" && calories > 0;
+	};
+
 	return (
 		<form className=" space-y5 bg-white shadow p-10 rounded-lg">
 			<div className=" grid grid-cols-1 gap-3">
@@ -70,8 +77,9 @@ export default function Form() {
 
 			<input
 				type="submit"
-				className=" bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+				className=" bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
 				value="Guardar Comida o ejercicio"
+				disabled={!isValidActivity()}
 			/>
 		</form>
 	);
