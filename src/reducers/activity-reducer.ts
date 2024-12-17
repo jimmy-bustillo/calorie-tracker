648e235db552ce3306f9716b9051fe19c240ab33
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2024-12-16 21:24:55 -0600

    Agrega actividad del useReducer

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,26 @@
+import type { Activity } from "../types";
+
+export type ActivityActions = {
+	type: "save-activity";
+	payload: { newActivity: Activity };
+};
+
+type ActivityState = {
+	activities: Activity[];
+};
+
+export const initialState: ActivityState = {
+	activities: [],
+};
+
+export const activityReducer = (
+	state: ActivityState = initialState,
+	action: ActivityActions,
+) => {
+	if (action.type === "save-activity") {
+		// Este código maneja la lógica para actualizar el state
+		console.log("Desde el type de save-activity");
+	}
+	
+	return state;
+};
